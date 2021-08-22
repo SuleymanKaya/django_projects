9574492c52b243e418ab09ffb6335f8c6ca22852
SuleymanKaya <skx.suleymankaya@gmail.com>
2021-08-22 22:12:12 +0300

    Yazilar modeli güncellendi

---
+++ blog/models/yazi.py
@@ -0,0 +1,25 @@
+from enum import unique
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import KategoriModel
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
+class YazilarModel(models.Model):
+    resim = models.ImageField(upload_to="yazi_resimleri")
+    baslik = models.CharField(max_length=50)
+    icerik = RichTextField()
+    olusturma_tarihi = models.DateTimeField(auto_now_add=True)
+    duzenleme_tarihi = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(populate_from="baslik", unique=True)
+    kategoriler = models.ManyToManyField(KategoriModel, related_name="yazi")
+    yazar = models.ForeignKey(User, on_delete=models.CASCADE, related_name="yazilar")
+
+    class Meta:
+        verbose_name = "Yazi"
+        verbose_name_plural = "Yazilar"
+        db_table = "yazi"
+
+    def __str__(self):
+        return self.baslik
+
