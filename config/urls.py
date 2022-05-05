e4e3f97ac993a786e41d7ed41f260ab63a2c0a8c
SuleymanKaya <skx.suleymankaya@gmail.com>
2022-05-05 17:49:39 +0200

    feat: jwt added

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/api/', include('post.api.urls', namespace='post')),
     path('comment/api/', include('comment.api.urls', namespace='comment')),
     path('favorite/api/', include('favorite.api.urls', namespace='favorite')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
--- favorite/api/views.py
+++ favorite/api/views.py
@@ -1,5 +1,5 @@
-from cgitb import lookup
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from favorite.api.paginations import FavoritePagination
 from favorite.api.permissions import IsOwner
 from favorite.api.serializers import FavoriteListCreateAPISerializer, FavoriteAPISerializer
@@ -8,6 +8,7 @@ from favorite.models import FavoriteModel
 class FavoriteListCreateAPIView(ListCreateAPIView):
     serializer_class = FavoriteListCreateAPISerializer
     pagination_class = FavoritePagination
+    permission_classes = [IsAuthenticated]
 
     # Sadece kullanıcıya ait değerleri getiren metod
     def get_queryset(self):
